d40a26f89a9525dfa8bcae3f1d6e1bee62e7074d
Zinzidane <ya-gans@mail.ru>
2018-09-05 16:12:44 +0300

    Добавляет компонент для создания профайла

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import  PropTypes from 'prop-types';
+import TextFieldGroup from '../common/TextFieldGroup';
+
+class CreateProfile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displaySocialInputs: false,
+      handle: '',
+      company: '',
+      website: '',
+      location: '',
+      status: '',
+      skills: '',
+      githubusername: '',
+      bio: '',
+      twitter: '',
+      youtube: '',
+      facebook: '',
+      linkedin: '',
+      vk: '',
+      errors: {}
+    };
+  }
+
+  render() {
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">Let's get some information to make your profile stand out!</p>
+              <small className="d-block p-3">* = required fields</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+CreateProfile.propType = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(CreateProfile);
